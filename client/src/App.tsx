467c11b43c454e5719d07f39e5ee5d8b5b345fe0
ugadimas25 <ugadimas@gmail.com>
2026-02-07 01:54:31 +0700

    Add BottomNav, GlobalAudioPlayer, ListeningLimitModal to App layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { AudioProvider } from "@/context/AudioContext";
+import { BottomNav } from "@/components/Navigation";
+import { GlobalAudioPlayer } from "@/components/AudioPlayer";
+import { ListeningLimitModal } from "@/components/ListeningLimitModal";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Explore from "@/pages/Explore";
@@ -34,6 +37,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AudioProvider>
         <Router />
+        <GlobalAudioPlayer />
+        <ListeningLimitModal />
+        <BottomNav />
         <Toaster />
       </AudioProvider>
     </QueryClientProvider>
